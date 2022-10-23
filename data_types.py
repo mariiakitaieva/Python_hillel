7efb94f0f14424c7e7d70a01855acd2aebd71454
Masha <kitaievam@ukr.net>
2022-10-23 16:46:01 +0300

    my first homework

---
+++ data_types.py
@@ -0,0 +1,11 @@
+first_variable = 1
+second_variable = 2
+third_variable = 3
+fourth_variable = 4
+fifth_variable = 5
+sixth_variable = "six"
+seventh_variable = "seven"
+eighth_variable = "eight"
+ninth_variable = "nine"
+tenth_variable = "ten"
+print(first_variable, second_variable, third_variable, fourth_variable, fifth_variable, sixth_variable, seventh_variable, eighth_variable, ninth_variable, tenth_variable)
