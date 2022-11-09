fd8bfaa77d3199df896cc10f4331e54df7021224
Masha <kitaievam@ukr.net>
2022-11-10 00:05:32 +0200

    my first homework

--- sets.py
+++ sets.py
@@ -4,21 +4,21 @@
 
 
 input_one = set(input("Enter whatever you want: "))
-print(type(input_one), input_one)
+#print(type(input_one), input_one)
 input_two = set(input("Enter whatever you want: "))
-print(type(input_two), input_two)
-input_three = input_one.symmetric_difference(input_two)
-print(input_three)
-input_four = input_one.intersection(input_two)
-print(input_four)
-#
+#print(type(input_two), input_two)
 #exit_letters = set()
 # exit_numbers =set()
 # for element in input_three:
 #     if element.isdigit():
 #         exit_numbers.add(element)
 # print(exit_numbers)
-exit_numbers = set([element for element in input_three if element.isdigit()])
+exit_numbers = set([element for element in input_one.symmetric_difference(input_two) if element.isdigit()])
 print(type(exit_numbers), exit_numbers)
-exit_letters = set([element for element in input_four if element.isalpha()])
+exit_letters = set([element for element in input_one.intersection(input_two) if element.isalpha()])
 print(type(exit_letters), exit_letters)
+'''Знайди та ознайомся з операторами |=, &=, -=, ^=, та методами які відповідають цим операторам. 
+Наведи по 1 прикладу для кожного оператора, використовуй 3 множини одночасно у якості операндів '''
+set_one = set(1, 2, 3, 11, 22)
+set_two = set(11, 4, 5)
+set_three = set(11, 8, 9)
