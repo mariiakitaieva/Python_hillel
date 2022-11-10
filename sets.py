2926706d51f4b2ab8513734d4fca59cbeab6a3c3
Masha <kitaievam@ukr.net>
2022-11-10 15:32:44 +0200

    my first homework

--- sets.py
+++ sets.py
@@ -17,8 +17,40 @@ exit_numbers = set([element for element in input_one.symmetric_difference(input_
 print(type(exit_numbers), exit_numbers)
 exit_letters = set([element for element in input_one.intersection(input_two) if element.isalpha()])
 print(type(exit_letters), exit_letters)
+
+
 '''Знайди та ознайомся з операторами |=, &=, -=, ^=, та методами які відповідають цим операторам. 
 Наведи по 1 прикладу для кожного оператора, використовуй 3 множини одночасно у якості операндів '''
-set_one = set(1, 2, 3, 11, 22)
-set_two = set(11, 4, 5)
-set_three = set(11, 8, 9)
+
+set_one = {1, 2, 3, 11, 22}
+set_two = {11, 4, 5}
+set_three = {11, 8, 9}
+#set_four = set_one | set_two | set_three
+#print(set_four)
+set_one |= set_two | set_three #union
+print(set_one)
+
+
+set_uno = {1, 2, 3, 11, 22}
+set_dos = {11, 4, 5}
+set_treis = {11, 8, 9}
+set_uno &= set_dos & set_treis
+print(set_uno)   #intersection
+
+
+set_un = {1, 2, 3, 11, 22}   #Результатом разности - является множество, содержащее элементы, которые есть в "уменьшаемом",
+ #но их нет в "вычитаемом". То есть уникальные для "уменьшаемого". Результат зависит от порядка операндов:
+set_deux = {11, 4, 5}
+set_trois = {11, 8, 9, 22}
+set_un -= set_deux
+print(set_un)
+
+
+#Результатом симметрической разности ^ (исключающего ИЛИ) является множество,
+# состоящее только из уникальных элементов исходных множеств. Совпадающие элементы исключаются:Eins, zwei, drei.
+set_eins = {1, 2, 3, 11, 22}
+set_zwei = {11, 4, 5}
+set_drei = {11, 8, 9}
+set_eins ^= set_zwei
+#set_one=set_one.symmetric_difference(set_three)
+print(set_eins)
