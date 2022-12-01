b2cae7d891fe73065a85d13a6b11cfb145a6e618
Masha <kitaievam@ukr.net>
2022-12-01 17:14:41 +0200

    my first homework

---
+++ functions3.py
@@ -0,0 +1,45 @@
+'''Надрукуй 4 рядки: помідор -> м'ясо -> сир -> хліб, умови:
+Створи 4 функції, кожна друкує свій шар
+Викликай лише одну функцію
+Використай синтаксис декораторів
+Очікуваний результат друку:
+помідор
+м'ясо
+сир
+хліб'''
+
+
+def tomato(func):
+    def wrapper():
+        print("помідор")
+        func()
+    return wrapper
+
+
+def meat(func):
+    def wrapper():
+        print("м'ясо")
+        func()
+    return wrapper
+
+
+def cheese(func):
+    def wrapper():
+        print("сир")
+        func()
+    return wrapper
+
+
+def food(bread):
+    print(bread)
+
+
+#sandwich = bread(ingredients(sandwich))
+#sandwich()
+
+@tomato
+@meat
+@cheese
+def food(bread = "хлiб"):
+    print(bread)
+food()
